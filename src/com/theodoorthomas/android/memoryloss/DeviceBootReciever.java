6d83403fffb973669f2b8ba109a035d00962b46d
Thomas Cremers <ttcremers@gmail.com>
2013-12-12 10:02:21 +0100

    added a receiver that listens for boot completion and start the background service

--- src/com/theodoorthomas/android/memoryloss/DeviceBootReciever.java
+++ src/com/theodoorthomas/android/memoryloss/DeviceBootReciever.java
@@ -1,21 +1,30 @@
 package com.theodoorthomas.android.memoryloss;
 
+import com.theodoorthomas.android.memoryloss.services.LogMonitService;
+
+import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
+import android.widget.FrameLayout;
 import android.widget.Toast;
 
 public class DeviceBootReciever extends BroadcastReceiver {
-
+	
 	@Override
 	public void onReceive(Context context, Intent intent) {
-		// TODO Auto-generated method stub
-		Log.d("STORAGEDEVICERECIEVER", "I recieved a message! " + intent.getAction());
-		if (intent.getAction().equalsIgnoreCase(
-		        "android.intent.action.UMS_CONNECTED")) {
-			Toast.makeText(context, "Device Connect recieved", Toast.LENGTH_LONG).show();
-		}
+		Log.i("MemoryLossService", "Starting MemoryLoss app monitor service!");
+		Intent monitService = new Intent(context, 
+				LogMonitService.class);
+		PendingIntent sender = PendingIntent.getService(context, 0, monitService, PendingIntent.FLAG_UPDATE_CURRENT);
+		AlarmManager am = (AlarmManager) context.getSystemService(Activity.ALARM_SERVICE);
+		am.cancel(sender); // cancel any existing alarms
+		// Math.round(AlarmManager.INTERVAL_FIFTEEN_MINUTES / 6)
+		am.setInexactRepeating(AlarmManager.RTC, System.currentTimeMillis(), 
+				10000, sender);
 	}
 
 }
